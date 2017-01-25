2c32ece4083fffef4046032a7a226b69ba68e1b2
ekshams <ekshamsudheen@gmail.com>
2017-01-25 19:39:11 +0530

    setting changed

--- mylibrary/settings.py
+++ mylibrary/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
+# if DEBUG:
     MEDIA_URL = '/media/'
 
     STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"mylibrary/static", "static-only")
